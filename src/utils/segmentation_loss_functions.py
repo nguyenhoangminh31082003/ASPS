3e5b670c45bf6d6303f4677f652d84d2e01a3464
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2025-01-10 15:57:25 +0700

    Move locations of model folder and the file containing the dice loss function definition

--- src/utils/segmentation_loss_functions.py
+++ src/utils/segmentation_loss_functions.py
@@ -1,6 +1,6 @@
 import torch
-import torch.nn.functional as F
 import torch.nn as nn
+import torch.nn.functional as F
 
 def dice_loss(pred, target, smooth=1):
     intersection = (pred * target).sum()
